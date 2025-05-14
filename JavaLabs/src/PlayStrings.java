c7bcd8f98104a08d8cc1c5d32aa7d102dd243ca8
write2ramakrishna@gmail.com <write2ramakrishna@gmail.com>
2025-05-13 22:37:37 -0500

    Testing

---
+++ JavaLabs/src/PlayStrings.java
@@ -0,0 +1,10 @@
+public class PlayStrings {
+    public static void main(String []args) {
+        String s = "India";
+
+        System.out.println(s.charAt(2));
+        System.out.println(s.substring(2));
+
+        System.out.println(s.substring(2,5));
+    }
+}
